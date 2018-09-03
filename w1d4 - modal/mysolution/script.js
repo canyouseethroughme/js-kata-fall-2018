47b15e0fdf7193d9147bec1ef6c2976da25eef60
Stefan Andrei <picotto@91.101.35.224.generic-hostname.arrownet.dk>
2018-09-03 17:49:38 +0200

    final modal

--- w1d4 - modal/mysolution/script.js
+++ w1d4 - modal/mysolution/script.js
@@ -1,9 +1,28 @@
 "use strict";
 
+const detailsButtons = document.querySelectorAll("article p");
+const modalWindow = document.querySelector("#modal_container");
+const modalH1 = document.querySelector("#modal_container h1");
+const closeModalButton = document.querySelector("#modal_container #closebutton");
+
+detailsButtons.forEach(detailButton => {
+  detailButton.addEventListener("click", showModal);
+});
+
+closeModalButton.addEventListener("click", hideModal);
+
+
+
+
+
 function showModal() {
   console.log("Show modal");
+  let detailsHeader = this.parentNode.childNodes[1].textContent;
+  modalH1.textContent = detailsHeader;
+  modalWindow.classList.remove("hidden");
 }
 
 function hideModal() {
   console.log("Hide modal");
+  modalWindow.classList.add("hidden");
 }
