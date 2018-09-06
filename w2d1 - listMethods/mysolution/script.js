560a2b522e138fe76c1148156c54b9c64ffaa078
Stefan Andrei <picotto@Stefans-MacBook-Pro.local>
2018-09-06 11:14:02 +0200

    listMethods done

--- w2d1 - listMethods/mysolution/script.js
+++ w2d1 - listMethods/mysolution/script.js
@@ -1,11 +1,41 @@
 "use strict";
 
-function displayElement( element ) {
-    console.log( element.textContent );
+function listMethods(id) {
+    let lis = document.querySelectorAll("#" + id + " li");
+    lis.forEach(li => {
+        displayElement(li);
+    });
 }
 
-listMethods("modify");
+function listMethodsFor(id) {
+    const lis = document.querySelectorAll("#" + id + " li");
+    for (let i = 0; i < lis.length; i++) {
+        displayElement(lis[i]);
+    }
+}
+
+
+
+
+function displayElement(element) {
+    console.log(element.textContent);
+}
+
 listMethods("info");
-listMethods("newarray");
+console.log("+++");
+listMethods("modify");
+
+console.log("+++");
+listMethods("newArray");
+
+console.log("=======");
+listMethodsFor("info");
+console.log("+++");
+listMethodsFor("modify");
+console.log("+++");
+listMethodsFor("newArray");
+
+
+
 
 // TODO: Create listMethods function
